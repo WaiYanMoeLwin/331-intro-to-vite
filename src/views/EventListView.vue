d34d851d4416694d2df486ced70462e6a46b1187
WaiYanMoeLwin <waiyanmoelwin1710@gmail.com>
2025-07-03 17:44:54 +0700

    finish 15.3

---
+++ src/views/EventListView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+import EventCategoryAndOrganizer from '@/components/EventCategoryAndOrganizer.vue';
+import type { Event } from '@/types';
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const events = ref<Event[]>([]);
+
+onMounted(() => {
+  axios
+    .get('https://my-json-server.typicode.com/WaiYanMoeLwin/331-lab-2-db/events')
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error("There was an error!", error);
+    });
+})
+
+</script>
+
+<template>
+  <h1>Events For Good</h1>
+  <!-- new element -->
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+  <div class="event-category-and-organizers">
+    <EventCategoryAndOrganizer v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.event-category-and-organizers {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+}
+</style>
